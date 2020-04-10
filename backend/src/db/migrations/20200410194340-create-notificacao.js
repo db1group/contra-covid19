a06cff9c92b5cb86b6c1fcad62aac5d65fbb37c6
Francisco Thiago <ftathiago@gmail.com>
2020-04-10 18:07:27 -0300

    Criar modelo de notificacao

---
+++ backend/src/db/migrations/20200410194340-create-notificacao.js
@@ -0,0 +1,60 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Notificacao', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+        autoIncrement: false,
+      },
+      userId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'User',
+          key: 'id',
+        },
+      },
+      unidadeId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'UnidadeSaude',
+          key: 'id',
+        },
+      },
+      notificadorId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'ProfissionalSaude',
+          key: 'id',
+        },
+      },
+      bairroId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'Bairro',
+          key: 'id',
+        },
+      },
+      pessoaId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'Pessoa',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Notificacao');
+  }
+};
---
+++ backend/src/models/notificacao.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Notificacao = sequelize.define('Notificacao', {
+    userId: DataTypes.UUID,
+    unidadeId: DataTypes.UUID,
+    notificadorId: DataTypes.UUID,
+    bairroId: DataTypes.UUID,
+    pessoaId: DataTypes.UUID
+  }, {
+    freezeTableName: false
+  });
+  Notificacao.associate = function (models) {
+    Notificacao.hasOne(models.Unidade);
+    Notificacao.hasOne(models.User);
+    Notificacao.hasOne(models.ProfissionalSaude, { foreignKey: 'notificadorId' });
+    Notificacao.hasOne(models.Bairro);
+    Notificacao.hasOne(models.Pessoa);
+  };
+  return Notificacao;
+};
