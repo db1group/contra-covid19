ef709c96b4a241bc5085d2cb18eef3e436ee1d59
Wilde <gutopicciani@yahoo.com.br>
2020-04-10 04:26:42 -0300

    Inserindo alguns campos no cadastro de notificação

---
+++ frontend/src/components/Notificacao/Form/IdentificacaoCaso.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h4 class="primary--text title">
+      1. IDENTIFICAÇÃO DO CASO SUSPEITO
+    </h4>
+    <v-container
+      fluid
+      class="pa-0"
+    >
+      <v-row>
+        <v-col cols="12">
+          <v-text-field
+            label="Nome completo *"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <label class="primary--text body-1">Sexo</label>
+          <v-radio-group class="mt-0">
+            <v-radio label="Masculino" value="M"/>
+            <v-radio label="Feminino" value="F"/>
+          </v-radio-group>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+  }),
+};
+</script>
---
+++ frontend/src/views/NotificacaoForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h3 class="primary--text my-7 display-1 text-center">
+      Cadastro de notificação
+    </h3>
+    <base-page>
+      <identificacao-caso/>
+    </base-page>
+  </section>
+</template>
+
+<script>
+import BasePage from '@/components/commons/BasePage.vue';
+import IdentificacaoCaso from '@/components/Notificacao/Form/IdentificacaoCaso.vue';
+
+export default {
+  components: {
+    BasePage,
+    IdentificacaoCaso,
+  },
+};
+</script>
