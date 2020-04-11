baf3b19d9ae5f1e2efb70f69be81087935922fd8
Francisco Thiago <ftathiago@gmail.com>
2020-04-11 16:40:07 -0300

    Criar Pesquisa por ID de notifcação

--- backend/src/resource/notificacao-resource.js
+++ backend/src/resource/notificacao-resource.js
@@ -1,14 +1,61 @@
 const models = require("../models");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
-const { mapearParaNotificacao, mapearParaRequest } = require("../mapper/notificacao-mapper");
+const { mapearParaNotificacao, mapearParaResponse } = require("../mapper/notificacao-mapper");
 const uuid = require("uuid/v4")
 
 exports.salvar = async (req, res) => {
-  const notificacaoId = uuid();
+
   let notificacao = mapearParaNotificacao(req.body);
 
-  notificacao = {
+  const notificacaoSalva = await salvarNotificacao(notificacao);
+
+  const retorno = mapearParaResponse(notificacaoSalva, notificacaoSalva.NotificacaoHistorico);
+
+  return res.json({
+    data: {
+      ...retorno,
+    }
+  })
+};
+
+exports.consultarPaginado = async (req, res) => {
+  const { page = 1 } = req.query;
+  const limit = 10;
+  const offset = (page - 1) * limit;
+  const notificacoes = await models.Notificacao.findAndCountAll({
+    include: [
+      { model: models.Pessoa },
+      { model: models.NotificacaoHistorico },
+    ],
+    order: [['updatedAt', 'DESC']],
+    limit: limit,
+    offset: offset
+  });
+  return res.json({ data: notificacoes });
+};
+
+exports.consultarPorId = async (req, res) => {
+  const { id } = req.params;
+  const notificacaoModel = await consultarNotificacaoPorId(id);
+
+  const retorno = mapearParaResponse(notificacaoModel, notificacaoModel.NotificacaoHistorico);
+
+  return res.json({ data: retorno });
+};
+
+const consultarNotificacaoPorId = async (id) =>
+  await models.Notificacao.findOne({
+    where: { id },
+    include: [
+      { model: models.Pessoa },
+      { model: models.NotificacaoHistorico }
+    ]
+  });
+
+const salvarNotificacao = async (notificacao) => {
+  const notificacaoId = uuid();
+  const notificacaoComId = {
     id: notificacaoId,
     ...notificacao,
     notificacaoHistorico: {
@@ -18,47 +65,7 @@ exports.salvar = async (req, res) => {
     }
   };
 
-  const notificacaoSalva = await models.Notificacao.create(notificacao);
-  const notificacaoHistoricoSalvo = await models.NotificacaoHistorico.create(notificacao.notificacaoHistorico);
-
-  const retorno = mapearParaRequest(notificacaoSalva, notificacaoHistoricoSalvo);
-
-  return res.json({
-    data: {
-      ...retorno,
-    }
-  })
-};
-
-exports.consultarPaginado = async (req, res) => {  
-  const { page = 1} = req.query;    
-  const limit = 10;
-  const offset = (page - 1) * limit;
-  const notificacoes = await models.Notificacao.findAndCountAll({  
-    include: [      
-      { model: models.Pessoa },      
-      { model: models.NotificacaoHistorico },
-    ],
-    order: [['updatedAt', 'DESC']],
-    limit: limit, 
-    offset: offset    
-  });
-  return res.json({data: notificacoes});  
-};
-
-exports.consultarPorId = async (req, res) => {  
-  const { id } = req.params;
-  const notificacaoModel = await models.Notificacao.findOne({
-    where: { id },
-    include: [
-      { model: models.Pessoa },
-      { model: models.NotificacaoHistorico }
-    ]
-  });
-
-  const { notificacaoHistorico, ...notificacao } = notificacaoModel;
-
-  const retorno = mapearParaRequest(notificacaoModel, notificacaoModel.NotificacaoHistorico);
-
-  return res.json({ data: retorno });
-};
+  await models.Notificacao.create(notificacaoComId);
+  await models.NotificacaoHistorico.create(notificacaoComId.notificacaoHistorico);
+  return await consultarNotificacaoPorId(notificacaoId);
+}
