7863aa2a4a74e0d0db7b907b67b9f25ec14294a6
Maykon Capellari <maykonluiscapellari@gmail.com>
2020-04-15 19:32:04 -0300

    adicionado validação em alguns campos pessoa

--- backend/src/models/pessoa.js
+++ backend/src/models/pessoa.js
@@ -1,32 +1,52 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Pessoa = sequelize.define(
-    "Pessoa",
-    {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-      },
-      nome: DataTypes.STRING(150),
-      dataDeNascimento: DataTypes.DATE,
-      sexo: DataTypes.ENUM("M", "F"),
-      idade: DataTypes.INTEGER,
-      numeroDocumento: DataTypes.STRING(18),
-      tipoDocumento: DataTypes.ENUM("CPF", "RG", "CNH", "SUS"),
-      nomeDaMae: DataTypes.STRING(150),
-      ocupacao: DataTypes.STRING(60),
-      endereco: DataTypes.STRING(150),
-      numero: DataTypes.STRING(12),
-      bairroId: DataTypes.UUID,
-      telefoneResidencial: DataTypes.STRING(12),
-      telefoneContato: DataTypes.STRING(12),
-      telefoneCelular: DataTypes.STRING(12),
+  const Pessoa = sequelize.define("Pessoa", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
-    {}
-  );
+    nome: {
+      type: DataTypes.STRING(150),
+      validate: {
+        isAlpha: true,
+        notEmpty: true,
+        len: [3, 150],
+      },
+    },
+    dataDeNascimento: {
+      type: DataTypes.DATE,
+      validate: {
+        isDate: true,
+        isNotGreaterTomorrow: function (value) {
+          const actualValue = new Date(value);
+          let tomorrow = new Date();
+          tomorrow = new Date(
+            tomorrow.getFullYear(),
+            tomorrow.getMonth(),
+            tomorrow.getDate()
+          );
+          if (actualValue >= tomorrow) {
+            throw new Error("Não pode informar uma data futura");
+          }
+        },
+      },
+    },
+    sexo: DataTypes.ENUM("M", "F"),
+    idade: DataTypes.INTEGER,
+    numeroDocumento: DataTypes.STRING(18),
+    tipoDocumento: DataTypes.ENUM("CPF", "RG", "CNH", "SUS"),
+    nomeDaMae: DataTypes.STRING(150),
+    ocupacao: DataTypes.STRING(60),
+    endereco: DataTypes.STRING(150),
+    numero: DataTypes.STRING(12),
+    bairroId: DataTypes.UUID,
+    telefoneResidencial: DataTypes.STRING(12),
+    telefoneContato: DataTypes.STRING(12),
+    telefoneCelular: DataTypes.STRING(12),
+  });
   Pessoa.associate = function (models) {
-    Pessoa.belongsTo(models.Bairro);
+    Pessoa.belongsTo(models.Bairro, { foreignKey: "bairroId" });
   };
   return Pessoa;
 };
