633dee32c4e2872c3eae9378733d6bae0a17383c
Francisco Thiago <ftathiago@gmail.com>
2020-04-15 21:33:44 -0300

    Apagar validações anteriores

--- backend/src/validations/user/request/index.js
+++
@@ -1,8 +0,0 @@
-const validationSchema = require("./validation-schema");
-
-const ValidateRequest = (request) => {
-    const validationResult = validationSchema.validate(request);
-    return validationResult;
-}
-
-module.exports = { ValidateRequest };
--- backend/src/validations/user/request/validation-schema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('@hapi/joi');
-
-module.exports = validationSchema = Joi.object({
-
-    email: Joi.string()
-        .email({ minDomainSegments: 1, tlds: { allow: ['com', 'net'] } })
-        .required()
-});
