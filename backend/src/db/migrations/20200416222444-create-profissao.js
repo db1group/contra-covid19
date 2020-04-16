d85c7309df4cd8f3ce9f1b3ac841c4861c1ab113
Francisco Thiago <ftathiago@gmail.com>
2020-04-16 20:04:15 -0300

    Criar Migrations para Profissao

---
+++ backend/src/db/migrations/20200416222444-create-profissao.js
@@ -0,0 +1,28 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Profissao", {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+        autoIncrement: false,
+      },
+      nome: {
+        type: Sequelize.STRING(60),
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Profissao");
+  },
+};
