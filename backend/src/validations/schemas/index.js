68ded6bb03f6a8911e56ec9acda7b894c65aee15
Francisco Thiago <ftathiago@gmail.com>
2020-04-18 00:11:17 -0300

    Criar validações para o endpoint de municipio

--- backend/src/validations/schemas/index.js
+++ backend/src/validations/schemas/index.js
@@ -1,9 +1,11 @@
 const pessoa = require('./pessoa');
 const user = require('./user');
 const notificacao = require('./notificacao');
+const municipio = require('./municipio');
 
 module.exports = {
   pessoa,
   user,
   notificacao,
+  municipio,
 };
