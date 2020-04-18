2dd1f0f14356518d69f54d2333ffc35869a184fc
Jefferson <araujosilva.jefferson@gmail.com>
2020-04-18 12:20:36 -0300

    model NotificacaoEvolucao

--- backend/src/models/notificacao.js
+++ backend/src/models/notificacao.js
@@ -56,6 +56,9 @@ module.exports = (sequelize, DataTypes) => {
     Notificacao.belongsTo(models.Profissao, {
       foreignKey: 'profissaoId',
     });
+    Notificacao.hasMany(models.NotificacaoEvolucao, {
+      foreignKey: 'notificacaoId',
+    });
   };
   return Notificacao;
 };
