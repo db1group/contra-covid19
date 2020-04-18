c8f3160fa8d9071ab90180c6068a8c56c3e9a0d6
Francisco Thiago <ftathiago@gmail.com>
2020-04-17 21:09:06 -0300

    Corrigir migrations

--- backend/src/db/migrations/20200417214428-adicionando-status-nomenotificador-notificacao.js
+++ backend/src/db/migrations/20200417214428-adicionando-status-nomenotificador-notificacao.js
@@ -1,17 +1,24 @@
 
 module.exports = {
-  up: (queryInterface, Sequelize) => Promise.all([
+  up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction((t) => Promise.all([
     queryInterface.addColumn('Notificacao', 'nomeNotificador', {
       type: Sequelize.STRING,
-    }),
+    }, { transaction: t }),
     queryInterface.addColumn('Notificacao', 'status', {
       type: Sequelize.STRING(9),
       allowNull: false,
       defaultValue: 'ABERTA',
-    }),
-  ]),
-  down: (queryInterface) => Promise.all([
-    queryInterface.removeColum('Notificacao', 'nomeNotificador'),
-    queryInterface.removeColum('Notificacao', 'status'),
-  ]),
+    }, { transaction: t }),
+  ])),
+  async down(queryInterface) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.removeColumn('Notificacao', 'nomeNotificador', { transaction });
+      await queryInterface.removeColumn('Notificacao', 'status', { transaction });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
 };
