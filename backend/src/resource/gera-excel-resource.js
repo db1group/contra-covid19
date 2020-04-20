2ddfd5ba2f0ba6b7d41ff992e350e6cfb07050a8
Ricardo Costa <ricardo.costa@db1.com.br>
2020-04-20 20:43:33 -0300

    Criação da exportação de notificação para excel #81

---
+++ backend/src/resource/gera-excel-resource.js
@@ -0,0 +1,138 @@
+const excel = require('excel4node');
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+const fs = require('fs');
+const moment = require('moment');
+
+exports.gerarExcel = async (colunas, lista, res) => {
+  try {
+    const diretorio = 'excel';
+    if (!fs.existsSync(diretorio)) {
+      fs.mkdirSync(diretorio);
+    }
+
+    const guid = uuidv4();
+    const nomeArquivo = `${diretorio}/${guid}.xlsx`;
+    const fullPath = path.resolve(nomeArquivo);
+
+    const wb = new excel.Workbook();
+    const ws = wb.addWorksheet('Planilha1');
+
+    if ((!lista || lista.length <= 0) && (!colunas || colunas.length <= 0)) {
+      wb.write(fullPath, res);
+      return;
+    }
+
+    const colunasTemp = colunas && colunas.length > 0 ? colunas : [];
+    if (!colunasTemp || colunasTemp.length <= 0) {
+      const element = lista[0];
+
+      const keys = Object.keys(element);
+      keys.forEach((key) => {
+        colunasTemp.push({
+          nomeColuna: key,
+          nomeCampo: key,
+        });
+      });
+    }
+
+    for (let index = 0; index < colunasTemp.length; index += 1) {
+      const element = colunasTemp[index];
+      ws.cell(1, index + 1).string(element.nomeColuna);
+    }
+
+    for (let index = 0; index < lista.length; index += 1) {
+      const item = lista[index];
+
+      for (let indice = 0; indice < colunasTemp.length; indice += 1) {
+        const nomeDoCampo = colunasTemp[indice].nomeCampo;
+
+        if (Object.prototype.hasOwnProperty.call(item, nomeDoCampo)) {
+          const value = item[nomeDoCampo];
+          if (typeof value === 'number') {
+            ws.cell(index + 2, indice + 1).number(value);
+          }
+
+          if (typeof value === 'string') {
+            ws.cell(index + 2, indice + 1).string(value);
+          }
+
+          if (typeof value === 'boolean') {
+            ws.cell(index + 2, indice + 1).bool(value);
+          }
+
+          if (value instanceof Date) {
+            ws.cell(index + 2, indice + 1).date(value).style({ numberFormat: 'dd/mm/yyyy hh:MM:ss' });
+          }
+        }
+      }
+    }
+
+    wb.write(fullPath, res);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ error: err.message });
+  }
+};
+
+this.retornarCampo = (objeto, nomeDaPropriedade) => {
+  if (!objeto) {
+    return null;
+  }
+
+  const objetoTemp = Object.prototype.hasOwnProperty.call(objeto, 'dataValues') ? objeto.dataValues : objeto;
+  if (!Object.prototype.hasOwnProperty.call(objetoTemp, nomeDaPropriedade)) {
+    return null;
+  }
+
+  return objetoTemp[nomeDaPropriedade];
+};
+
+exports.retornarHoraDaData = (objeto, nomeDaPropriedade) => {
+  if (!objeto) {
+    return null;
+  }
+
+  const objetoTemp = Object.prototype.hasOwnProperty.call(objeto, 'dataValues') ? objeto.dataValues : objeto;
+  if (!Object.prototype.hasOwnProperty.call(objetoTemp, nomeDaPropriedade)) {
+    return null;
+  }
+
+  const data = objetoTemp[nomeDaPropriedade];
+  const horasFuso = data instanceof Date ? data.getTimezoneOffset() / 60 : 0;
+
+  return moment(data).locale('pt-BR').add(horasFuso, 'hours').format('LT');
+};
+
+exports.retornarDataSemHora = (objeto, nomeDaPropriedade) => {
+  if (!objeto) {
+    return null;
+  }
+
+  const objetoTemp = Object.prototype.hasOwnProperty.call(objeto, 'dataValues') ? objeto.dataValues : objeto;
+  if (!Object.prototype.hasOwnProperty.call(objetoTemp, nomeDaPropriedade)) {
+    return null;
+  }
+
+  const data = objetoTemp[nomeDaPropriedade];
+  const horasFuso = data instanceof Date ? data.getTimezoneOffset() / 60 : 0;
+
+  return moment(data).add(horasFuso, 'hours').format('DD/MM/YYYY');
+};
+
+exports.preencherCampoBoolean = (objeto, nomeDaPropriedade) => {
+  if (!objeto) {
+    return null;
+  }
+
+  const objetoTemp = Object.prototype.hasOwnProperty.call(objeto, 'dataValues') ? objeto.dataValues : objeto;
+  if (!Object.prototype.hasOwnProperty.call(objetoTemp, nomeDaPropriedade)) {
+    return null;
+  }
+
+  if (!(typeof objetoTemp[nomeDaPropriedade] === 'boolean')) {
+    return null;
+  }
+
+  return objetoTemp[nomeDaPropriedade] ? 'Sim' : 'Não';
+};
