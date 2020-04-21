817d9e6d9e708f205777803b3bca32e8a045555c
Francisco Thiago <ftathiago@gmail.com>
2020-04-21 03:04:09 -0300

    Criar validação para POST /profissões

--- backend/src/validations/schemas/profissao.js
+++ backend/src/validations/schemas/profissao.js
@@ -1,8 +1,12 @@
 const Joi = require('@hapi/joi');
 
+const TAMANHO_CAMPO_NOME = 60;
 const schemas = {
   consultaPorNome: Joi.object().keys({
-    nome: Joi.string().max(60),
+    nome: Joi.string().max(TAMANHO_CAMPO_NOME),
+  }),
+  cadastrar: Joi.object().keys({
+    nome: Joi.string().max(TAMANHO_CAMPO_NOME),
   }),
 };
 module.exports = schemas;
