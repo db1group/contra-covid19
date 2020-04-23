3ecb79d273a56dc015b0667e584415fcd3c56f4a
Francisco Thiago <ftathiago@gmail.com>
2020-04-22 22:39:42 -0300

    Realocar arquivo de manipulação de erros

--- backend/src/lib/erros/api-error-handling.js
+++ backend/src/lib/erros/api-error-handling.js
@@ -1,4 +1,5 @@
 const { ValidationError } = require('sequelize');
+const RegraNegocio = require('./RegraNegocio');
 
 const tratarErrorsRetornoAPI = (response, erro) => {
   let codigoResposta = 500;
@@ -6,8 +7,15 @@ const tratarErrorsRetornoAPI = (response, erro) => {
     error: erro.message,
     stack: process.env.NODE_ENV === 'development' ? erro.stack : null,
   };
+
   if (erro instanceof ValidationError) {
     const errorMessage = erro.errors.map((erros) => erros.message).join(',\n');
+    codigoResposta = 400;
+    objetoRetorno.error = errorMessage;
+  }
+
+  if (erro instanceof RegraNegocio) {
+    const errorMessage = erro.message;
     codigoResposta = 400;
     objetoRetorno.error = errorMessage;
   }
