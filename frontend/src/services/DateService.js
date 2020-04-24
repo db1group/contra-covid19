b6dd8489e44a9f5e29a1fa04565ab6aac0c606db
Fabio Bezerra de Souza <bsouza.fabio@gmail.com>
2020-04-24 14:16:17 -0400

    Implementando validações de data

--- frontend/src/services/DateService.js
+++ frontend/src/services/DateService.js
@@ -17,7 +17,98 @@ export default {
   toMomentZoneObject(stringDate, format) {
     return moment.parseZone(stringDate, format);
   },
+  formatStringTypeToDateTypeWithMinutes(stringDate) {
+    const splitedDateTime = stringDate.split(' ');
+    const splitedDate = splitedDateTime[0].split('/');
+    const splitedTime = splitedDateTime[1].split(':');
+
+    const inputedDateTime = new Date(splitedDate[2], splitedDate[1], splitedDate[0],
+      splitedTime[0], splitedTime[1], 0);
+
+    return inputedDateTime;
+  },
+  formatDateTypeToStringTypeWithMinutes(date) {
+    const inputedDateTime = moment(date).format('DD/MM/YYYY HH:mm');
+
+    return inputedDateTime;
+  },
   isDateValid(stringDate, format) {
     return moment(stringDate, format).isValid();
+  },
+  isLessThanMaximumDate(stringInformedDate, stringMaximumDate) {
+    const splitedInformedValue = stringInformedDate.split('/');
+    const inputedInformedDate = new Date(`
+      ${splitedInformedValue[2]}-${splitedInformedValue[1]}-${splitedInformedValue[0]}
+    `);
+
+    let maximumDate;
+    if (stringMaximumDate) {
+      const splitedMaximumValue = stringMaximumDate.split('/');
+      maximumDate = new Date(`
+        ${splitedMaximumValue[2]}-${splitedMaximumValue[1]}-${splitedMaximumValue[0]}
+      `);
+    } else {
+      maximumDate = new Date();
+    }
+
+    return (inputedInformedDate < maximumDate);
+  },
+  isGreaterThanMinimumDate(stringInformedDate, stringMinimumDate) {
+    const splitedInformedValue = stringInformedDate.split('/');
+    const inputedInformedDate = new Date(`
+      ${splitedInformedValue[2]}-${splitedInformedValue[1]}-${splitedInformedValue[0]}
+    `);
+
+    let minimumDate;
+    if (stringMinimumDate) {
+      const splitedMinimumValue = stringMinimumDate.split('/');
+      minimumDate = new Date(`
+        ${splitedMinimumValue[2]}-${splitedMinimumValue[1]}-${splitedMinimumValue[0]}
+      `);
+    } else {
+      minimumDate = new Date();
+    }
+
+    return (inputedInformedDate > minimumDate);
+  },
+  isLessThanMaximumDateWithMinutes(stringInformedDate, stringMaximumDate) {
+    const splitedDateTime = stringInformedDate.split(' ');
+    const splitedDateValue = splitedDateTime[0].split('/');
+    const splitedTimeValue = splitedDateTime[1].split(':');
+    const inputedInformedDate = new Date(splitedDateValue[2], splitedDateValue[1], splitedDateValue[0],
+      splitedTimeValue[0], splitedTimeValue[1], 0);
+
+    let maximumDate;
+    if (stringMaximumDate) {
+      const splitedMaximumDateTime = stringMaximumDate.split(' ');
+      const splitedMaximumDateValue = splitedMaximumDateTime[0].split('/');
+      const splitedMaximumTimeValue = splitedMaximumDateTime[1].split(':');
+      maximumDate = new Date(splitedMaximumDateValue[2], splitedMaximumDateValue[1], splitedMaximumDateValue[0],
+        splitedMaximumTimeValue[0], splitedMaximumTimeValue[1], 0);
+    } else {
+      maximumDate = new Date();
+    }
+
+    return (inputedInformedDate < maximumDate);
+  },
+  isGreaterThanMinimumDateWithMinutes(stringInformedDate, stringMinimumDate) {
+    const splitedDateTime = stringInformedDate.split(' ');
+    const splitedDateValue = splitedDateTime[0].split('/');
+    const splitedTimeValue = splitedDateTime[1].split(':');
+    const inputedInformedDate = new Date(splitedDateValue[2], splitedDateValue[1], splitedDateValue[0],
+      splitedTimeValue[0], splitedTimeValue[1], 0);
+
+    let minimumDate;
+    if (stringMinimumDate) {
+      const splitedMinimumDateTime = stringMinimumDate.split(' ');
+      const splitedMinimumDateValue = splitedMinimumDateTime[0].split('/');
+      const splitedMinimumTimeValue = splitedMinimumDateTime[1].split(':');
+      minimumDate = new Date(splitedMinimumDateValue[2], splitedMinimumDateValue[1], splitedMinimumDateValue[0],
+        splitedMinimumTimeValue[0], splitedMinimumTimeValue[1], 0);
+    } else {
+      minimumDate = new Date();
+    }
+
+    return (inputedInformedDate > minimumDate);
   },
 };
