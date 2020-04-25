4662f4ed1e03719810c875b5e7be0bb770d97293
Altieres de Matos <altieres.matos@db1.com.br>
2020-04-25 14:02:33 -0300

    #320 - Resolvendo migration que altera campo para not-null

--- backend/src/db/migrations/20200424223213-alterar-notificacaoCovid19-dataInicioSintomas-DATEONLY.js
+++ backend/src/db/migrations/20200424223213-alterar-notificacaoCovid19-dataInicioSintomas-DATEONLY.js
@@ -2,7 +2,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.changeColumn('NotificacaoCovid19', 'dataInicioDosSintomas', {
     type: Sequelize.DATEONLY,
-    allowNull: false,
   }),
   down: (queryInterface, Sequelize) => queryInterface.changeColumn('NotificacaoCovid19', 'dataInicioDosSintomas',
     {
