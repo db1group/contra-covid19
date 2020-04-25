6421431022526c88cb7df6f27b8bd7b5b82d7209
Ricardo Costa <ricardo.costa@db1.com.br>
2020-04-25 19:50:31 -0300

    Retirado o defaultValue de campo nullable

--- backend/src/db/migrations/20200425222300-alterar-campo-tipoLaboratorio-para-nullable.js
+++ backend/src/db/migrations/20200425222300-alterar-campo-tipoLaboratorio-para-nullable.js
@@ -4,7 +4,6 @@ module.exports = {
     queryInterface.changeColumn('NotificacaoCovid19', 'tipoLaboratorio', {
       type: Sequelize.STRING(12),
       allowNull: true,
-      defaultValue: '',
     }),
   ]),
   down: (queryInterface, Sequelize) => Promise.all([
