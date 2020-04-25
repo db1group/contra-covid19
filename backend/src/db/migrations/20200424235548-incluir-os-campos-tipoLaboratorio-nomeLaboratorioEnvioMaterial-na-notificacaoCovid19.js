7c51beb582cd5efa51caa49d65c0d14e7501d84a
Ricardo Costa <ricardo.costa@db1.com.br>
2020-04-25 01:48:10 -0300

    Ajustes para rodar undo

--- backend/src/db/migrations/20200424235548-incluir-os-campos-tipoLaboratorio-nomeLaboratorioEnvioMaterial-na-notificacaoCovid19.js
+++ backend/src/db/migrations/20200424235548-incluir-os-campos-tipoLaboratorio-nomeLaboratorioEnvioMaterial-na-notificacaoCovid19.js
@@ -7,23 +7,19 @@ module.exports = {
         type: Sequelize.STRING(12),
         allowNull: false,
         defaultValue: '',
-      });
-      await queryInterface.addColumn('NotificacaoCovid19', 'nomeLaboratorioEnvioMaterial', { type: Sequelize.STRING(30) });
+      },
+      { transaction });
+
+      await queryInterface.addColumn('NotificacaoCovid19', 'nomeLaboratorioEnvioMaterial', { type: Sequelize.STRING(30) }, { transaction });
       await transaction.commit();
     } catch (err) {
       await transaction.rollback();
       throw err;
     }
   },
+
   async down(queryInterface) {
-    const transaction = await queryInterface.sequelize.transaction();
-    try {
-      queryInterface.removeColumn('NotificacaoCovid19', 'tipoLaboratorio', { transaction });
-      queryInterface.removeColumn('NotificacaoCovid19', 'nomeLaboratorioEnvioMaterial', { transaction });
-      await transaction.commit();
-    } catch (err) {
-      await transaction.rollback();
-      throw err;
-    }
+    await queryInterface.removeColumn('NotificacaoCovid19', 'tipoLaboratorio');
+    await queryInterface.removeColumn('NotificacaoCovid19', 'nomeLaboratorioEnvioMaterial');
   },
 };
