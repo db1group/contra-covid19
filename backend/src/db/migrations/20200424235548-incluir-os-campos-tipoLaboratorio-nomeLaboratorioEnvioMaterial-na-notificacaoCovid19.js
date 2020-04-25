ad1641f3b27bb92dba16b5a500d85e28ac9ab752
Ricardo Costa <ricardo.costa@db1.com.br>
2020-04-25 01:17:51 -0300

    Alterado forma de salvar informações do laboratório #213

---
+++ backend/src/db/migrations/20200424235548-incluir-os-campos-tipoLaboratorio-nomeLaboratorioEnvioMaterial-na-notificacaoCovid19.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.addColumn('NotificacaoCovid19', 'tipoLaboratorio', { type: Sequelize.STRING(12) });
+      await queryInterface.addColumn('NotificacaoCovid19', 'nomeLaboratorioEnvioMaterial', { type: Sequelize.STRING(30) });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+  async down(queryInterface) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      queryInterface.removeColumn('NotificacaoCovid19', 'tipoLaboratorio', { transaction });
+      queryInterface.removeColumn('NotificacaoCovid19', 'nomeLaboratorioEnvioMaterial', { transaction });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+};
