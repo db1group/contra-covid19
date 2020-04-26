3a46d3e60dcfa40435cfa96d9fb2beec45bfc3bb
Francisco Thiago <ftathiago@gmail.com>
2020-04-25 23:28:06 -0300

    Adicionar validação para nomeNotificador

--- backend/src/validations/schemas/notificacao.js
+++ backend/src/validations/schemas/notificacao.js
@@ -2,24 +2,27 @@ const Joi = require('@hapi/joi');
 
 const schemas = {
   cadastrar: Joi.object().keys({
-    // userId: Joi.string().guid({ version: 'uuidv4' }).required(),
+    // dataHoraNotificacao: Joi.date().iso().required(),
     // unidadeSaudeId: Joi.string().guid({ version: 'uuidv4' }).required(),
     // notificadorId: Joi.string().guid({ version: 'uuidv4' }).required(),
-    // profissaoId: Joi.string().guid({ version: 'uuidv4' }).required(),
-    // dataHoraNotificacao: Joi.date().iso(),
-    // sintomatico: Joi.bool(),
+    // sintomatico: Joi.bool().required(),
+    // realizouExamesImagem: Joi.bool(),
     // dataInicioDosSintomas: Joi.date().iso(),
+    // userId: Joi.string().guid({ version: 'uuidv4' }).required(),
     nomeNotificador: Joi.string().required().min(3).max(80),
+    // profissaoId: Joi.string().guid({ version: 'uuidv4' }).required(),
+    // tipoDeContatoComCaso: Joi.string(),
+    // tipoDeLocalDoCaso: Joi.string(),
+    // descricaoDoLocalDoCaso: Joi.string().allow(''),
+    // nomeDoCaso: Joi.string(),
     // observacoes: Joi.string(),
     // suspeito: Joi.object(),
     // sintomas: Joi.object(),
     // comorbidades: Joi.object(),
+    // examesImagem: Joi.object(),
     // informacaoComplementar: Joi.object(),
     // vinculoEpidemiologico: Joi.object(),
     // conclusaoAtendimento: Joi.object(),
-    // status: Joi.string()
-    //   .pattern(/ABERTA|ENCERRADA|EXCLUIDA/)
-    //   .required(),
   }),
 };
 module.exports = schemas;
