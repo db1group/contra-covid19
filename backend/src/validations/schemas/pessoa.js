0218ff7a534131e8c83948aa06a089d5d4e94bf4
Francisco Thiago <ftathiago@gmail.com>
2020-04-27 20:41:55 -0300

    Adicionar validações ao request de pessoa

--- backend/src/validations/schemas/pessoa.js
+++ backend/src/validations/schemas/pessoa.js
@@ -5,7 +5,10 @@ const schemas = {
     nome: Joi.string().min(3).max(150).required(),
     dataDeNascimento: Joi.date().iso(),
     sexo: Joi.string().regex(/M|F/),
-    idade: Joi.number().integer().min(1).max(123),
+    idade: Joi.number().integer()
+      .allow(null)
+      .min(1)
+      .max(123),
     numeroDocumento: Joi.string()
       .pattern(/^[0-9]{3,18}$/)
       .required(),
@@ -16,11 +19,21 @@ const schemas = {
     ocupacao: Joi.string().max(60),
     endereco: Joi.string().max(150),
     numero: Joi.string().max(18),
-    bairroId: Joi.string().guid({ version: 'uuidv4' }),
-    municicpioId: Joi.string().guid({ version: 'uuidv4' }),
+    bairroId: Joi.string()
+      .guid({ version: 'uuidv4' })
+      .required(),
+    municipioId: Joi.string()
+      .guid({ version: 'uuidv4' })
+      .required(),
     telefoneResidencial: Joi.string().max(18),
     telefoneContato: Joi.string().max(18),
     telefoneCelular: Joi.string().max(18),
+    gestante: Joi.string()
+      .pattern(/SIM|NAO|NAO_APLICADO/),
+    racaCor: Joi.string()
+      .pattern(/BRANCA|PRETA|AMARELA|PARDA|INDIGENA|IGNORADO/),
+    complemento: Joi.string().allow(null, ''),
+    ocupacaoId: Joi.string().guid().required(),
   }),
   consultarId: Joi.object().keys({
     id: Joi.string().guid({ version: 'uuidv4' }),
