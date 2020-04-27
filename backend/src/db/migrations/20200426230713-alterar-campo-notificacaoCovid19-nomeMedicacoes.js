9350a435c38d5dd70cfd14e51979db118d4f5858
Francisco Thiago <ftathiago@gmail.com>
2020-04-26 23:15:32 -0300

    Atualiza Banco de dados com tamanhos reais

---
+++ backend/src/db/migrations/20200426230713-alterar-campo-notificacaoCovid19-nomeMedicacoes.js
@@ -0,0 +1,36 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.changeColumn('NotificacaoCovid19', 'nomeMedicacaoAntitermica',
+        { type: Sequelize.DataTypes.STRING(120) }, { transaction });
+      await queryInterface.changeColumn('NotificacaoCovid19', 'nomeMedicacaoAnalgesica',
+        { type: Sequelize.DataTypes.STRING(120) }, { transaction });
+      await queryInterface.changeColumn('NotificacaoCovid19', 'nomeMedicacaoAntiflamatorio',
+        { type: Sequelize.DataTypes.STRING(120) }, { transaction });
+      await queryInterface.changeColumn('NotificacaoCovid19', 'nomeMedicacaoAntiviral',
+        { type: Sequelize.DataTypes.STRING(120) }, { transaction });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.changeColumn('NotificacaoCovid19', 'nomeMedicacaoAntitermica',
+        { type: Sequelize.DataTypes.STRING }, { transaction });
+      await queryInterface.changeColumn('NotificacaoCovid19', 'nomeMedicacaoAnalgesica',
+        { type: Sequelize.DataTypes.STRING }, { transaction });
+      await queryInterface.changeColumn('NotificacaoCovid19', 'nomeMedicacaoAntiflamatorio',
+        { type: Sequelize.DataTypes.STRING }, { transaction });
+      await queryInterface.changeColumn('NotificacaoCovid19', 'nomeMedicacaoAntiviral',
+        { type: Sequelize.DataTypes.STRING }, { transaction });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
+  },
+};
