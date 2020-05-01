05038a3f675d9394fb946499c83ffb8f768cd67d
Francisco Thiago <ftathiago@gmail.com>
2020-05-01 12:52:24 -0300

    Configurar para aplicações javascript

--- backend/sonar-project.js
+++ backend/sonar-project.js
@@ -4,11 +4,14 @@ module.exports = sonarqubeScanner(
   {
     serverUrl: 'http://localhost:9000',
     options: {
+      'sonar.language': 'js',
       'sonar.sources': 'src',
       'sonar.tests': '__tests__',
-      'sonar.inclusions': 'src/**', // Entry point of your code
+      'sonar.inclusions': 'src/**',
+      'sonar.exclusions': 'src/db/**',
       'sonar.test.inclusions': '__tests__/**/*.test.js',
       'sonar.javascript.lcov.reportPaths': '__tests__/coverage/lcov.info',
+      'sonar.eslint.reportPaths': '__tests__/coverage/eslint-report.info',
       'sonar.testExecutionReportPaths': '__tests__/coverage/test-reporter.xml',
     },
   }, () => { },
