422dfc93759df60327b11f5834be1e1ee5bd6074
Francisco Thiago <ftathiago@gmail.com>
2020-05-03 14:07:43 -0300

    Refatorar métods de mapeamento para classe

--- backend/src/mapper/notificacao-mapper/model-para-response.js
+++ backend/src/mapper/notificacao-mapper/model-para-response.js
@@ -1,292 +1,285 @@
-const notificacaoParaResponse = (notificacao, notificacaoCovid19, request) => {
-  const {
-    nomeNotificador, unidadeSaudeId, notificadorId, userId, profissaoId, UnidadeSaude,
-  } = notificacao;
-  const {
-    dataHoraNotificacao, dataInicioDosSintomas, sintomatico,
-    realizouExameDeImagem, contatoComSuspeito, localDoContatoComSuspeito, nomeSuspeito,
-    observacoes,
-  } = notificacaoCovid19;
-  return {
-    id: notificacao.id,
-    dataHoraNotificacao,
-    unidadeSaudeId,
-    unidadeSaudeNome: UnidadeSaude.nome,
-    notificadorId,
-    sintomatico,
-    realizouExamesImagem: realizouExameDeImagem,
-    dataInicioDosSintomas,
-    userId,
-    nomeNotificador,
-    profissaoId,
-    tipoDeContatoComCaso: contatoComSuspeito,
-    tipoDeLocalDoCaso: localDoContatoComSuspeito,
-    nomeDoCaso: nomeSuspeito,
-    observacoes,
-    ...request,
-  };
-};
 
-const extrairSuspeitoDaNotificacao = ({
-  pessoaId, bairroId, municipioId, profissaoId, ocupacaoId,
-}) => ({
-  pessoaId,
-  bairroId,
-  municipioId,
-  profissaoId,
-  ocupacaoId,
-});
+class NotificacaoResponseMapper {
+  constructor(notificacao, notificacaoCovid19) {
+    this.notificacao = { ...notificacao };
+    this.notificacaoCovid19 = { ...notificacaoCovid19 };
+  }
 
-const extrairSuspeitoDaPessoa = ({
-  id,
-  tipoDocumento,
-  numeroDocumento,
-  nome,
-  dataDeNascimento,
-  sexo,
-  idade,
-  bairroId,
-  nomeDaMae,
-  ocupacaoId,
-  ocupacao,
-  endereco,
-  numero,
-  telefoneResidencial,
-  telefoneContato,
-  telefoneCelular,
-  complemento,
-  Bairro,
-  gestante,
-  tipoPeriodoGestacional,
-  racaCor,
-  Municipio,
-  cep,
-  tipoClassificacaoPessoa,
-}, bairro) => ({
-  pessoaId: id,
-  tipoDocumento,
-  numeroDocumento,
-  nome,
-  dataDeNascimento,
-  sexo,
-  cep,
-  bairroId,
-  bairro: bairro ? bairro.nome : Bairro.nome,
-  nomeDaMae,
-  ocupacao,
-  ocupacaoId,
-  endereco,
-  numero,
-  complemento,
-  municipioId: Municipio.id,
-  municipio: Municipio ? Municipio.nome : '',
-  telefoneResidencial,
-  telefoneContato,
-  telefoneCelular,
-  racaCor,
-  tipoClassificacaoPessoa,
-  uf: Municipio ? Municipio.uf : 'PR',
-  idade,
-  gestante,
-  tipoPeriodoGestacional,
-});
+  pegarResponse() {
+    const notificacao = this._notificacaoParaResponse();
+    const suspeito = this._extrairSuspeito();
+    const sintomas = this._extrairSintomas();
+    const comorbidades = this._extrairComorbidades();
+    const examesImagem = this._extrairExamesImagem();
+    const informacaoComplementar = this._extrairInformacaoComplementar();
+    const vinculoEpidemiologico = this._extrairVinculoEpidemiologico();
+    const conclusaoAtendimento = this._extrairConclusaoAtendimento();
+    return {
+      ...notificacao,
+      suspeito,
+      sintomas,
+      comorbidades,
+      examesImagem,
+      informacaoComplementar,
+      vinculoEpidemiologico,
+      conclusaoAtendimento,
+    };
+  }
 
-const extrairSuspeito = (notificacao) => {
-  const { Pessoa, Bairro } = notificacao;
-  if (Pessoa) { return extrairSuspeitoDaPessoa(Pessoa, Bairro); }
-  return extrairSuspeitoDaNotificacao(notificacao);
-};
+  /** métodos privados */
+  _notificacaoParaResponse() {
+    const {
+      nomeNotificador, unidadeSaudeId, notificadorId, userId, profissaoId,
+      UnidadeSaude,
+    } = this.notificacao;
+    const {
+      dataHoraNotificacao, dataInicioDosSintomas, sintomatico, realizouExameDeImagem,
+      contatoComSuspeito, localDoContatoComSuspeito, nomeSuspeito, observacoes,
+    } = this.notificacaoCovid19;
+    return {
+      id: this.notificacao.id,
+      dataHoraNotificacao,
+      unidadeSaudeId,
+      unidadeSaudeNome: UnidadeSaude.nome,
+      notificadorId,
+      sintomatico,
+      realizouExamesImagem: realizouExameDeImagem,
+      dataInicioDosSintomas,
+      userId,
+      nomeNotificador,
+      profissaoId,
+      tipoDeContatoComCaso: contatoComSuspeito,
+      tipoDeLocalDoCaso: localDoContatoComSuspeito,
+      nomeDoCaso: nomeSuspeito,
+      observacoes,
+    };
+  }
 
-const extrairSintomas = (notificacaoCovid19) => {
-  const {
-    febreAferidaReferida,
-    temperaturaFebre,
-    adinamiaFraqueza,
-    artralgia,
-    calafrios,
-    conjuntivite,
-    coriza,
-    congestaoNasal,
-    dificuldadeDeglutir,
-    gangliosLinfaticos,
-    irritabilidadeConfusao,
-    manchasVermelhas,
-    tosse,
-    dorDeGarganta,
-    mialgia,
-    escarro,
-    sibilo,
-    batimentoAsasNasais,
-    dispneia,
-    taquipneia,
-    saturacaoDeOximetriaDePulso,
-    cianoseCentral,
-    diminuicaoDePulsoPeriferico,
-    hipotensao,
-    diarreia,
-    cefaleia,
-    nauseaVomito,
-    tiragemIntercostal,
-    outrosSintomas,
-  } = notificacaoCovid19;
-  return {
-    febreAferidaReferida,
-    temperaturaFebre,
-    adinamiaFraqueza,
-    artralgia,
-    calafrios,
-    conjuntivite,
-    coriza,
-    congestaoNasal,
-    dificuldadeDeglutir,
-    gangliosLinfaticos,
-    irritabilidadeOuConfusao: irritabilidadeConfusao,
-    manchasVermelhas,
-    tosse,
-    dorDeGarganta,
-    mialgia,
-    escarro,
-    sibilo,
-    batimentoAsasNasais,
-    dispneia,
-    taquipneia,
-    saturacaoDeOximetriaDePulso,
-    cianoseCentral,
-    diminuicaoDePulsoPeriferico,
-    hipotensao,
-    diarreia,
-    cefaleia,
-    nauseaVomito,
-    tiragemIntercostal,
-    outros: outrosSintomas,
-  };
-};
+  _extrairSuspeito() {
+    const { Pessoa, Bairro } = this.notificacao;
+    if (Pessoa) { return this._extrairSuspeitoDaPessoa(Pessoa, Bairro); }
+    return this._extrairSuspeitoDaNotificacao(this.notificacao);
+  }
 
-const extrairComorbidades = (notificacaoCovid19) => {
-  const {
-    puerperaAte45DiasDoParto,
-    sindromeDeDown,
-    diabetesMellitus,
-    imunodeficiencia,
-    doencaCardioVascularCronica,
-    doencaHepaticaCronica,
-    doencaNeurologicaCronica,
-    doencaRenalCronica,
-    doencaHematologicaCronica,
-    asma,
-    outraPneumopatiaCronica,
-    obesidade,
-    hipertensao,
-    infeccaoHIV,
-    neoplasia,
-    tabagismo,
-    outrosComorbidades,
-  } = notificacaoCovid19;
-  return {
-    puerperaAte45DiasDoParto,
-    sindromeDeDown,
-    diabetesMellitus,
-    imunodeficiencia,
-    doencaCardioVascularCronica,
-    doencaHepaticaCronica,
-    doencaNeurologicaCronica,
-    doencaRenalCronica,
-    doencaHematologicaCronica,
-    asma,
-    outraPneumopatiaCronica,
-    obesidade,
-    hipertensao,
-    infeccaoHIV,
-    neoplasia,
-    tabagismo,
-    outros: outrosComorbidades,
-  };
-};
+  _extrairSuspeitoDaPessoa({
+    id,
+    tipoDocumento,
+    numeroDocumento,
+    nome,
+    dataDeNascimento,
+    sexo,
+    idade,
+    bairroId,
+    nomeDaMae,
+    ocupacaoId,
+    ocupacao,
+    endereco,
+    numero,
+    telefoneResidencial,
+    telefoneContato,
+    telefoneCelular,
+    complemento,
+    Bairro,
+    gestante,
+    tipoPeriodoGestacional,
+    racaCor,
+    Municipio,
+    cep,
+    tipoClassificacaoPessoa,
+  }, bairro) {
+    return {
+      pessoaId: id,
+      tipoDocumento,
+      numeroDocumento,
+      nome,
+      dataDeNascimento,
+      sexo,
+      cep,
+      bairroId,
+      bairro: bairro ? bairro.nome : Bairro.nome,
+      nomeDaMae,
+      ocupacao,
+      ocupacaoId,
+      endereco,
+      numero,
+      complemento,
+      municipioId: bairro ? bairro.municipioId : Bairro.id,
+      municipio: Municipio ? Municipio.nome : '',
+      telefoneResidencial,
+      telefoneContato,
+      telefoneCelular,
+      racaCor,
+      tipoClassificacaoPessoa,
+      uf: Municipio ? Municipio.uf : 'PR',
+      idade,
+      gestante,
+      tipoPeriodoGestacional,
+    };
+  }
 
-const extrairInformacaoComplementar = (notificacaoCovid19) => {
-  const {
-    tamiflu,
-    hidroxicloroquina,
-    nomeMedicamento,
-    historicoDeViagem,
-    dataDaViagem,
-    localDaViagem,
-    recebeuVacinaDaGripeNosUltimosDozeMeses,
-  } = notificacaoCovid19;
-  return {
-    tamiflu,
-    hidroxicloroquina,
-    nomeMedicamento,
-    historicoDeViagem,
-    dataDaViagem,
-    localDaViagem,
-    recebeuVacinaDaGripeNosUltimosDozeMeses,
-  };
-};
+  _extrairSuspeitoDaNotificacao({
+    pessoaId, bairroId, municipioId, profissaoId, ocupacaoId,
+  }) {
+    return {
+      pessoaId,
+      bairroId,
+      municipioId,
+      profissaoId,
+      ocupacaoId,
+    };
+  }
 
-const extrairVinculoEpidemiologico = (notificacaoCovid19) => {
-  const {
-    situacao1,
-    situacao2,
-    nomeTeveContato,
-  } = notificacaoCovid19;
-  return {
-    situacao1,
-    situacao2,
-    nome: nomeTeveContato,
-  };
-};
+  _extrairSintomas() {
+    const {
+      febreAferidaReferida, temperaturaFebre, adinamiaFraqueza, artralgia, calafrios,
+      conjuntivite, coriza, congestaoNasal, dificuldadeDeglutir, gangliosLinfaticos,
+      irritabilidadeConfusao, manchasVermelhas, tosse, dorDeGarganta, mialgia, escarro,
+      sibilo, batimentoAsasNasais, dispneia, taquipneia, saturacaoDeOximetriaDePulso,
+      cianoseCentral, diminuicaoDePulsoPeriferico, hipotensao, diarreia, cefaleia,
+      nauseaVomito, tiragemIntercostal, outrosSintomas,
+    } = this.notificacaoCovid19;
+    return {
+      febreAferidaReferida,
+      temperaturaFebre,
+      adinamiaFraqueza,
+      artralgia,
+      calafrios,
+      conjuntivite,
+      coriza,
+      congestaoNasal,
+      dificuldadeDeglutir,
+      gangliosLinfaticos,
+      irritabilidadeOuConfusao: irritabilidadeConfusao,
+      manchasVermelhas,
+      tosse,
+      dorDeGarganta,
+      mialgia,
+      escarro,
+      sibilo,
+      batimentoAsasNasais,
+      dispneia,
+      taquipneia,
+      saturacaoDeOximetriaDePulso,
+      cianoseCentral,
+      diminuicaoDePulsoPeriferico,
+      hipotensao,
+      diarreia,
+      cefaleia,
+      nauseaVomito,
+      tiragemIntercostal,
+      outros: outrosSintomas,
+    };
+  }
 
-const extrairConclusaoAtendimento = (notificacaoCovid19) => {
-  const {
-    coletaMaterialParaDiagnostico,
-    tipoLaboratorio,
-    nomeLaboratorioEnvioMaterial,
-    situacaoNoMomentoDaNotificacao,
-    dataDaColeta,
-    metodoDeExame,
-  } = notificacaoCovid19;
-  return {
-    situacaoNoMomentoDaNotificacao,
-    coletaMaterialParaDiagnostico,
-    tipoLaboratorio,
-    nomeLaboratorioEnvioMaterial,
-    dataDaColeta,
-    metodoDeExame,
-  };
-};
+  _extrairComorbidades() {
+    const {
+      puerperaAte45DiasDoParto,
+      sindromeDeDown,
+      diabetesMellitus,
+      imunodeficiencia,
+      doencaCardioVascularCronica,
+      doencaHepaticaCronica,
+      doencaNeurologicaCronica,
+      doencaRenalCronica,
+      doencaHematologicaCronica,
+      asma,
+      outraPneumopatiaCronica,
+      obesidade,
+      hipertensao,
+      infeccaoHIV,
+      neoplasia,
+      tabagismo,
+      outrosComorbidades,
+    } = this.notificacaoCovid19;
+    return {
+      puerperaAte45DiasDoParto,
+      sindromeDeDown,
+      diabetesMellitus,
+      imunodeficiencia,
+      doencaCardioVascularCronica,
+      doencaHepaticaCronica,
+      doencaNeurologicaCronica,
+      doencaRenalCronica,
+      doencaHematologicaCronica,
+      asma,
+      outraPneumopatiaCronica,
+      obesidade,
+      hipertensao,
+      infeccaoHIV,
+      neoplasia,
+      tabagismo,
+      outros: outrosComorbidades,
+    };
+  }
 
-const extrairExamesImagem = ({
-  raioXNormal,
-  raioXInfiltrado,
-  raioXConsolidacao,
-  raioXMisto,
-  raioXOutro,
-  tomografiaNormal,
-  tomografiaVitro,
-  tomografiaDerrame,
-  tomografiaLinfonodo,
-  tomografiaOutro,
-}) => ({
-  raioNormal: raioXNormal,
-  raioInfiltradoIntersticial: raioXInfiltrado,
-  raioConsolidacao: raioXConsolidacao,
-  raioMisto: raioXMisto,
-  raioOutro: raioXOutro,
-  tomografiaNormal,
-  tomografiaVidroFoscoPredominioPerifericoBasal: tomografiaVitro,
-  tomografiaAusenciaDerramePleural: tomografiaDerrame,
-  tomografiaAusenciaLinfonodoMediastenal: tomografiaLinfonodo,
-  tomografiaOutro,
-});
+  _extrairExamesImagem() {
+    const {
+      raioXNormal, raioXInfiltrado, raioXConsolidacao, raioXMisto, raioXOutro,
+      tomografiaNormal, tomografiaVitro, tomografiaDerrame, tomografiaLinfonodo,
+      tomografiaOutro,
+    } = this.notificacaoCovid19;
+    return {
+      raioNormal: raioXNormal,
+      raioInfiltradoIntersticial: raioXInfiltrado,
+      raioConsolidacao: raioXConsolidacao,
+      raioMisto: raioXMisto,
+      raioOutro: raioXOutro,
+      tomografiaNormal,
+      tomografiaVidroFoscoPredominioPerifericoBasal: tomografiaVitro,
+      tomografiaAusenciaDerramePleural: tomografiaDerrame,
+      tomografiaAusenciaLinfonodoMediastenal: tomografiaLinfonodo,
+      tomografiaOutro,
+    };
+  }
 
-module.exports = {
-  notificacaoParaResponse,
-  extrairSuspeito,
-  extrairSintomas,
-  extrairComorbidades,
-  extrairExamesImagem,
-  extrairInformacaoComplementar,
-  extrairVinculoEpidemiologico,
-  extrairConclusaoAtendimento,
-};
+  _extrairInformacaoComplementar() {
+    const {
+      tamiflu,
+      hidroxicloroquina,
+      nomeMedicamento,
+      historicoDeViagem,
+      dataDaViagem,
+      localDaViagem,
+      recebeuVacinaDaGripeNosUltimosDozeMeses,
+    } = this.notificacaoCovid19;
+    return {
+      tamiflu,
+      hidroxicloroquina,
+      nomeMedicamento,
+      historicoDeViagem,
+      dataDaViagem,
+      localDaViagem,
+      recebeuVacinaDaGripeNosUltimosDozeMeses,
+    };
+  }
+
+  _extrairVinculoEpidemiologico() {
+    const { situacao1, situacao2, nomeTeveContato } = this.notificacaoCovid19;
+    return {
+      situacao1,
+      situacao2,
+      nome: nomeTeveContato,
+    };
+  }
+
+  _extrairConclusaoAtendimento() {
+    const {
+      coletaMaterialParaDiagnostico,
+      tipoLaboratorio,
+      nomeLaboratorioEnvioMaterial,
+      situacaoNoMomentoDaNotificacao,
+      dataDaColeta,
+      metodoDeExame,
+    } = this.notificacaoCovid19;
+    return {
+      situacaoNoMomentoDaNotificacao,
+      coletaMaterialParaDiagnostico,
+      tipoLaboratorio,
+      nomeLaboratorioEnvioMaterial,
+      dataDaColeta,
+      metodoDeExame,
+    };
+  }
+}
+
+module.exports = NotificacaoResponseMapper;
