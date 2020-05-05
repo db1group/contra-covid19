3a9d4fa986cd8a62d147bc0b513165f19cb70ed0
Maykon Capellari <maykonluiscapellari@gmail.com>
2020-05-05 20:15:12 -0300

    adicionado keycloak nas rotas

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 const path = require('path');
 
 const basename = path.basename(__filename);
+const secure = require('../secure');
 
 module.exports = (app) => {
+  const keycloak = secure(app);
   fs.readdirSync(__dirname)
     .filter((file) => (
       file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
@@ -12,7 +14,7 @@ module.exports = (app) => {
     .forEach((file) => {
       // eslint-disable-next-line import/no-dynamic-require
       const route = require(path.join(__dirname, file));
-      app.use('/api', route);
+      app.use('/api', keycloak.protect(), route);
     });
   // Endpoint para o Docker/AWS
   app.use(require('./health'));
