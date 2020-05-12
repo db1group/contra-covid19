772574e69b267781763f1181631e0db19d38d9d3
Wilde <gutopicciani@yahoo.com.br>
2020-05-12 01:08:32 -0300

    Ajustes gerais no header

--- frontend/src/components/commons/UnidadeHeader.vue
+++ frontend/src/components/commons/UnidadeHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <v-row class="unidade-header" align="center" dense>
     <img
-      src="@/assets/icon-covid.png"
+      src="@/assets/icon-notification.svg"
       alt="Notifica Saúde"
-      class="mr-2"
+      class="mr-2 unidade-header__logo"
     />
     <v-col
-      cols="10"
+      class="unidade-header__info"
     >
       <v-row dense>
         <v-col cols="12">
@@ -14,7 +14,7 @@
         </v-col>
         <v-col cols="12">
           <div class="body-2 unidade-header__full-name">
-            {{ fullName }}
+            {{ fullName }} Amet amet labore id voluptate officia nostrud laboris eu enim nisi cupidatat.
           </div>
         </v-col>
       </v-row>
@@ -24,11 +24,21 @@
 <style lang="sass" scoped>
 @import ~@/styles/mobileFirst
 .unidade-header
-  min-width: 350px
+  &__logo
+    width: 40px
+  &__info
+    display: none
+    width: calc(100% - 48px)
+    white-space: nowrap
+    overflow: hidden
+    text-overflow: ellipsis
   &__full-name
     white-space: nowrap
     overflow: hidden
     text-overflow: ellipsis
+  +media-min-sm
+    &__info
+      display: block
 </style>
 <script>
 import KeycloakService from '@/services/KeycloakService';
