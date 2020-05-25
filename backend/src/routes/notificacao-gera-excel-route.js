46ca523bff53669a9de6b6125c6a954906a11a60
Ricardo Costa <ricardo.costa@db1.com.br>
2020-05-25 10:08:48 -0300

    Removido as classes antigas de geração de excel ##591

--- backend/src/routes/notificacao-gera-excel-route.js
+++ backend/src/routes/notificacao-gera-excel-route.js
@@ -1,15 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-const NotificacaoExportacaoExcelResource = require('../resource/notificacao-exportacao-excel-resource');
-const NotificacaoExportacaoExcelNovoResource = require('../resource/gera-excel-notificacao-resource');
+const NotificacaoExportacaoExcelResource = require('../resource/gera-excel-notificacao-resource');
 const secure = require('../secure');
 const { isRealmSecretariaSaude } = require('../lib/secureRealm');
 
 const keycloack = secure();
 
 router.get('/notificacao-gera-excel', keycloack.protect(isRealmSecretariaSaude), NotificacaoExportacaoExcelResource.gerarExcel);
-
-router.get('/notificacao-gera-excel/novo', keycloack.protect(isRealmSecretariaSaude), NotificacaoExportacaoExcelNovoResource.gerarExcel);
 
 module.exports = router;
