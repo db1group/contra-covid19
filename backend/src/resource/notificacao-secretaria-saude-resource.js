2fd02d2e0a9202ca5ed7e7fc14d13f7624d8f3aa
Alexandre Santos Graca de Andrade <alexandre.andrade@db1.com.br>
2020-05-25 01:30:06 -0300

    rota para enviar notificação para secretaria

--- backend/src/resource/notificacao-secretaria-saude-resource.js
+++ backend/src/resource/notificacao-secretaria-saude-resource.js
@@ -1,13 +1,21 @@
-const enviarNotificacaoSecretariaSaudeService = require('../services/enviar-notificacao-secretaria-saude-service');
+const secretariaApi = require('../services-api/secretaria-saude/service');
+const repos = require('../repositories/repository-factory');
+const EnviarNotificacaoRequest = require('../services-api/secretaria-saude/models/enviar-notificacao-request');
+const { RegraNegocioErro } = require('../lib/erros');
 
 exports.enviarNotificacao = async (req, res, next) => {
     try {
         const { id } = req.params;
 
         const notificacao = await repos.notificacaoRepository.getPorId(id);
-        
-        await enviarNotificacaoSecretariaSaudeService.handle(id);
-        return res.json();
+        const request = new EnviarNotificacaoRequest(notificacao);
+        const response = await secretariaApi.enviarNotificacao(request);
+
+        if (response.statusCode !== 200) {
+            throw new RegraNegocioErro(response.data);
+        }
+
+        return res.json({ data: response });
     } catch (err) {
         return next(err);
     }
