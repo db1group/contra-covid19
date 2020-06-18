fab2ee52fd5896bc0a5d01a1a0d7f6d5caa588f9
Maykon Capellari <maykonluiscapellari@gmail.com>
2020-06-19 00:48:18 +0100

    aumentado memória heap e do container

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,4 +11,4 @@ echo 'Starting seeders...'
 npx sequelize-cli db:seed:all
 
 echo 'Starting server...'
-pm2-runtime start /app/src/server.js -i max --max-memory-restart 300M --node-args="--max_old_space_size=300"
+pm2-runtime start /app/src/server.js -i max --node-args="--max_old_space_size=2048"
