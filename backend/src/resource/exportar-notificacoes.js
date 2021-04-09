954d5c06e4c0a2e78c0266bdc69d22499b8fac83
ramos <claudioramos@maringa.pr.gov.br>
2021-04-09 14:12:41 -0300

    Refactoring; Separando layout

--- backend/src/resource/exportar-notificacoes.js
+++ backend/src/resource/exportar-notificacoes.js
@@ -1,34 +1,9 @@
-const { Readable, Transform } = require('stream');
+const { Readable } = require('stream');
 const ExportaNotificacaoRepository = require('../repositories/exporta-notificacao-repository');
 const Stopwatch = require('../lib/stopwatch');
 const { UsuarioLogado } = require('../secure/usuario-logado');
-const { RegraNegocioErro } = require('../lib/erros');
-
-const transformCSV = () => {
-  let headerSeted = false;
-  return new Transform({
-    readableObjectMode: true,
-    writableObjectMode: true,
-    transform(chunk, _encoding, callback) {
-      let data = '';
-      if (!headerSeted) {
-        const cabecalhos = ExportaNotificacaoRepository
-          .cabecalhosExportacao
-          .map((n) => n.header).join(',');
-        data += `${cabecalhos}\n`;
-        headerSeted = true;
-      }
-      const dataCSV = Object.entries(chunk)
-        .map(([_, value]) => (value
-          ? `"${value.toString().replace(/"/g, '\'')}"`
-          : ''))
-        .join(',');
-
-      data += `${dataCSV}\n`;
-      callback(null, data);
-    },
-  });
-};
+const ExportaCSVPrevia = require('./ExportaCSVPrevia');
+const ExportaCSV = require('./ExportaCSV');
 
 const createReadableStream = (notificacoes) => new Readable({
   objectMode: true,
@@ -45,19 +20,16 @@ exports.exportarNotificacoes = async (req, res, next) => {
       dataInicialFiltro, dataFinalFiltro, dataEvolucaoInicialFiltro, dataEvolucaoFinalFiltro,
     ] = ExportaNotificacaoRepository.retornarFiltrosData(req.query);
     const { tenant } = new UsuarioLogado(req);
-    const [notificacoes] = await ExportaNotificacaoRepository.consultarNotificacoes(
-      dataInicialFiltro, dataFinalFiltro, dataEvolucaoInicialFiltro, dataEvolucaoFinalFiltro,
-      tenant,
-    );
-    if (notificacoes.length === 0) throw new RegraNegocioErro('Não existem notificações neste período.');
-
-    const rows = ExportaNotificacaoRepository.retornarRowsNotificacaoExcel(notificacoes);
+    const { previa } = req.query;
+    const exportador = previa === 'SIM' ? new ExportaCSVPrevia() : new ExportaCSV();
+    const rows = await exportador.fetchRows(dataInicialFiltro, dataFinalFiltro,
+      dataEvolucaoInicialFiltro, dataEvolucaoFinalFiltro, tenant);
     const notificacoesStream = createReadableStream(rows);
     res.on('finish', () => {
       sw.stop();
       sw.summary();
     });
-    notificacoesStream.pipe(transformCSV()).pipe(res);
+    notificacoesStream.pipe(exportador.transformaCSV()).pipe(res);
   } catch (err) {
     console.error(err);
     next(err);
